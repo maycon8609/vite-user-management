92fc0784b55f35c83199b78899a49391230d82cc
maycon silva <maycon8609@gmail.com>
2024-07-22 15:29:37 -0300

    feat: add EditUser component

---
+++ src/components/EditUser/EditUser.tsx
@@ -0,0 +1,69 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { User } from "../../contexts";
+
+interface EditeUserProps {
+  cancelEdit: () => void;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSaveChange: () => void;
+  user: User | null;
+}
+
+export const EditUser = ({
+  cancelEdit,
+  handleChange,
+  handleSaveChange,
+  user,
+}: EditeUserProps) => {
+  return (
+    <Dialog open={!!user} onClose={cancelEdit}>
+      <DialogTitle>Edit User</DialogTitle>
+      <DialogContent>
+        {user && (
+          <>
+            <TextField
+              margin="dense"
+              label="Name"
+              name="name"
+              fullWidth
+              value={user.name}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              label="Email"
+              name="email"
+              fullWidth
+              value={user.email}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              label="Password"
+              name="password"
+              type="password"
+              fullWidth
+              value={user.password}
+              onChange={handleChange}
+            />
+          </>
+        )}
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={cancelEdit} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleSaveChange} color="primary">
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
