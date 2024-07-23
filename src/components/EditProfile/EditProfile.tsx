55570065d734d06a0c430861b420ffa448e5eab4
maycon silva <maycon8609@gmail.com>
2024-07-23 12:54:19 -0300

    refactor: update EditProfile

--- src/components/EditProfile/EditProfile.tsx
+++ src/components/EditProfile/EditProfile.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -10,80 +10,105 @@ import {
 } from "@mui/material";
 
 import { User } from "../../types";
+import { useAuth, useUserManagement } from "../../hooks";
 
 interface EditProfileProps {
-  handleClose: () => void;
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  handleSaveChange: () => void;
-  user: User | null;
+  onClose: () => void;
   isOpen: boolean;
 }
 
-export const EditProfile: FC<EditProfileProps> = ({
-  handleClose,
-  handleChange,
-  handleSaveChange,
-  user,
-  isOpen,
-}) => {
+export const EditProfile: FC<EditProfileProps> = ({ onClose, isOpen }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const { loggedUser } = useAuth();
+  const { updateUser } = useUserManagement();
+
+  useEffect(() => {
+    setUser(loggedUser);
+  }, [loggedUser]);
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+
+    if (user) {
+      setUser({ ...user, [name]: value });
+    }
+  };
+
+  const handleSaveChange = () => {
+    if (user) {
+      updateUser(user);
+      onClose();
+    }
+  };
+
+  const handleCloseDialog = () => {
+    onClose();
+    setUser(loggedUser);
+  };
+
   return (
-    <Dialog open={isOpen} onClose={handleClose}>
-      <Box
-        sx={{ m: 1, mt: 2 }}
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <img
-          srcSet="https://picsum.photos/200?random=1"
-          src="https://picsum.photos/200?random=1"
-          alt="Imagem de perfil do usuario"
-          loading="lazy"
-          width={80}
-          height={80}
-          style={{
-            borderRadius: "50%",
-          }}
-        />
-        <Typography component="h1" variant="h5">
-          {user!.name}
-        </Typography>
-      </Box>
-      <DialogContent>
-        {user && (
-          <>
-            <TextField
-              margin="dense"
-              label="Nome"
-              name="name"
-              fullWidth
-              value={user.name}
-              onChange={handleChange}
+    <Dialog open={isOpen} onClose={onClose}>
+      {user && (
+        <>
+          <Box
+            sx={{ m: 1, mt: 2 }}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <img
+              srcSet="https://picsum.photos/200?random=1"
+              src="https://picsum.photos/200?random=1"
+              alt="Imagem de perfil do usuario"
+              loading="lazy"
+              width={80}
+              height={80}
+              style={{
+                borderRadius: "50%",
+              }}
             />
-            <TextField
-              margin="dense"
-              label="E-mail"
-              name="email"
-              fullWidth
-              value={user.email}
-              onChange={handleChange}
-            />
-            <TextField
-              margin="dense"
-              label="Senha"
-              name="password"
-              type="password"
-              fullWidth
-              value={user.password}
-              onChange={handleChange}
-            />
-          </>
-        )}
-      </DialogContent>
+            <Typography component="h1" variant="h5">
+              {user!.name}
+            </Typography>
+          </Box>
+          <DialogContent>
+            <>
+              <TextField
+                margin="dense"
+                label="Nome"
+                name="name"
+                fullWidth
+                value={user.name}
+                onChange={handleChange}
+              />
+              <TextField
+                margin="dense"
+                label="E-mail"
+                name="email"
+                fullWidth
+                value={user.email}
+                onChange={handleChange}
+              />
+              <TextField
+                margin="dense"
+                label="Senha"
+                name="password"
+                type="password"
+                fullWidth
+                value={user.password}
+                onChange={handleChange}
+              />
+            </>
+          </DialogContent>
+        </>
+      )}
 
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
+        <Button onClick={handleCloseDialog} color="primary">
           Cancelar
         </Button>
         <Button onClick={handleSaveChange} color="primary">
