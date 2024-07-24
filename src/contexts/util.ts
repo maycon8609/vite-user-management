8934a18929ab06daf99b41cda81ba3d93d13b963
maycon silva <maycon8609@gmail.com>
2024-07-24 19:19:26 -0300

    chore: update generate mock script

--- src/contexts/util.ts
+++ src/contexts/util.ts
@@ -1,21 +1,22 @@
+import { faker } from '@faker-js/faker'
+
 import { User } from "../types"
 
 const users: User[] = [
-  { id: Math.random().toString(36).substring(2), email: "adm@email.com", name: "adm", password: '123456', type: "ADMIN" },
-  { id: Math.random().toString(36).substring(2), email: "levi.ackerman@google.com", name: "Levi ackerman", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "gon.freecss@google.com", name: "Gon Freecss", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "killua.zoldyck@google.com", name: "Killua Zoldyck", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "yusuke.urameshi@google.com", name: "	Yusuke Urameshi", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "roronoa.zoro@google.com", name: "Roronoa Zoro", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "vegeta@google.com", name: "Vegeta IV", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "ichigo.kurosaki@google.com", name: "Ichigo Kurosaki", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "cloud.strife@gmail.com", name: "Cloud Strife", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "light.yagami@hotmail.com", name: "Light Yagami", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "link.hero@gmail.com", name: "Link", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "arthas.menethil@yahoo.com", name: "Arthas Menethil", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "guts.berserk@outlook.com", name: "Guts", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "geralt.rivia@google.com", name: "Geralt of Rivia", password: Math.random().toString(36).substring(2), type: "USER" },
-  { id: Math.random().toString(36).substring(2), email: "aloy.horizon@outlook.com", name: "Aloy", password: Math.random().toString(36).substring(2), type: "USER" },
+  { id: faker.string.uuid(), email: "admin@email.com", name: "administrador", password: '123', type: "ADMIN" },
+  { id: faker.string.uuid(), email: "levi.ackerman@google.com", name: "Levi ackerman", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "gon.freecss@google.com", name: "Gon Freecss", password: faker.internet.password(), type: "ADMIN" },
+  { id: faker.string.uuid(), email: "killua.zoldyck@google.com", name: "Killua Zoldyck", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "yusuke.urameshi@google.com", name: "	Yusuke Urameshi", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "roronoa.zoro@google.com", name: "Roronoa Zoro", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "ichigo.kurosaki@google.com", name: "Ichigo Kurosaki", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "cloud.strife@gmail.com", name: "Cloud Strife", password: faker.internet.password(), type: "ADMIN" },
+  { id: faker.string.uuid(), email: "light.yagami@hotmail.com", name: "Light Yagami", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "link.hero@gmail.com", name: "Link", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "arthas.menethil@yahoo.com", name: "Arthas Menethil", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "guts.berserk@outlook.com", name: "Guts", password: faker.internet.password(), type: "ADMIN" },
+  { id: faker.string.uuid(), email: "geralt.rivia@google.com", name: "Geralt of Rivia", password: faker.internet.password(), type: "USER" },
+  { id: faker.string.uuid(), email: "aloy.horizon@outlook.com", name: "Aloy", password: faker.internet.password(), type: "USER" },
 ];
 
 
